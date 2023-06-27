2bdf60a809bc5b34bcf253ba397270fd90c480dd
Jeanluca <jeanlucafp@gmail.com>
2023-06-27 08:41:51 -0300

    Update E2ETests.java

--- infra/src/test/java/br/com/carteira/infra/E2ETests.java
+++ infra/src/test/java/br/com/carteira/infra/E2ETests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,6 +17,7 @@ import org.testcontainers.utility.DockerImageName;
 @Testcontainers
 @AutoConfigureMockMvc
 @SpringBootTest(classes = Main.class)
+@ActiveProfiles("test")
 @Tag("e2e-test")
 public class E2ETests {
 
