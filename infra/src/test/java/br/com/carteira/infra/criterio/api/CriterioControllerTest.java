1af706434848e8536c812ad113c5a9f73f178f10
Jeanluca <jeanlucafp@gmail.com>
2023-07-19 10:22:46 -0300

    Update CriterioControllerTest.java

--- infra/src/test/java/br/com/carteira/infra/criterio/api/CriterioControllerTest.java
+++ infra/src/test/java/br/com/carteira/infra/criterio/api/CriterioControllerTest.java
@@ -5,6 +5,11 @@ import br.com.carteira.infra.E2ETests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -13,6 +18,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @DisplayName("Criterio APi Test")
 class CriterioControllerTest extends E2ETests {
+
+    @Container
+    public static MongoDBContainer MONGO_CONTAINER = new MongoDBContainer(DockerImageName.parse("mongo:6.0.5"));
+
+    @DynamicPropertySource
+    public static void mongoDbProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", MONGO_CONTAINER::getReplicaSetUrl);
+    }
 
     @Test
     @DisplayName("deve trazer os criterios corretos")
