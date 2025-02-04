3596c70ec945efc3f02c69481a55273758e9280f
Jeanluca <jeanlucafp@gmail.com>
2025-02-04 08:28:38 -0300

    add UsuarioController with get endpoint and corresponding tests

--- infra/src/main/java/br/com/carteira/infra/usuario/api/UsuarioController.java
+++ infra/src/main/java/br/com/carteira/infra/usuario/api/UsuarioController.java
@@ -1,4 +1,23 @@
 package br.com.carteira.infra.usuario.api;
 
+import br.com.carteira.infra.usuario.mongodb.Usuario;
+import br.com.carteira.infra.usuario.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public Usuario get(@RequestParam(name = "name", defaultValue = "") String name,
+                       @RequestParam(name = "email", defaultValue = "") String email) {
+        return usuarioService.needUsuario(name, email);
+    }
 }
